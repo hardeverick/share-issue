4b82bec10ea27bde637d237289656859b8ddab12
Erick Del Orbe <erick@har.com>
2022-12-12 11:31:58 -0600

    Added share example

--- ShareIssue/ViewController.swift
+++ ShareIssue/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func didTapShareButton(_ sender: Any) {
+        let testUrlString = "https://www.har.com/homedetail/19522-concho-springs-dr-katy-tx-77449/3673239?cid=BOGANYS"
+        
+        if let testUrl = URL(string: testUrlString) {
+            let avc = UIActivityViewController(activityItems: [testUrl], applicationActivities: nil)
+            self.present(avc, animated: true)
+        }
+    }
 }
 
